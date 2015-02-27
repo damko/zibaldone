bf358387409f69a90efc8359d24de56b4fd21ecf
damko <d.venturin@squadrainformatica.com>
2015-02-27 01:23:36 +0100

    updated sql file

--- database.sql
+++ database.sql
@@ -1,17 +1,24 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";# MySQL returned an empty result set (i.e. zero rows).
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-SET time_zone = "+00:00";# MySQL returned an empty result set (i.e. zero rows).
-
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `thumbnail` varchar(255) DEFAULT NULL,
+  `full_filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `dir` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;# MySQL returned an empty result set (i.e. zero rows).
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `downloads` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -27,8 +34,7 @@ CREATE TABLE IF NOT EXISTS `downloads` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `full_filename` (`full_filename`),
   KEY `reference_id` (`reference_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;# MySQL returned an empty result set (i.e. zero rows).
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `fragments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -46,8 +52,7 @@ CREATE TABLE IF NOT EXISTS `fragments` (
   KEY `reference_id` (`reference_id`),
   KEY `full_filename` (`full_filename`),
   KEY `type` (`type`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;# MySQL returned an empty result set (i.e. zero rows).
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `github_references` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -58,8 +63,7 @@ CREATE TABLE IF NOT EXISTS `github_references` (
   `sha` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `reference_id` (`reference_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;# MySQL returned an empty result set (i.e. zero rows).
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `references` (
   `id` varchar(32) NOT NULL,
@@ -70,4 +74,19 @@ CREATE TABLE IF NOT EXISTS `references` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   UNIQUE KEY `id` (`id`),
   KEY `book_id` (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;# MySQL returned an empty result set (i.e. zero rows).
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `related_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_id` int(11) NOT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `article_id` int(11) DEFAULT NULL,
+  `bookmark_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
